5f66529f21f26f10d3c1e3e9297fc13766e02f92
tyler-ray90 <tyler_ray98@yahoo.com>
2021-05-18 14:05:12 -0400

    Added model and migration

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    description = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
--- todos/urls.py
+++ todos/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
